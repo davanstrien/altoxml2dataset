3701e6e14a972ac1ada703c21cee980d9263898f
Daniel van Strien <davanstrien@gmail.com>
2022-10-24 20:12:02 +0100

    process_script

---
+++ load.py
@@ -0,0 +1,63 @@
+import subprocess
+from pathlib import Path
+
+from datasets import concatenate_datasets
+from toolz import concat
+from tqdm.auto import tqdm
+
+from alto2dataset.europena import *
+
+europena_ids = [9200396, 9200301]
+
+all_datasets = []
+Path("altodata").mkdir(exist_ok=True)
+Path("altodata/metadata").mkdir(exist_ok=True)
+for id_ in tqdm(europena_ids):
+    subprocess.call(
+        [
+            "aria2c",
+            "-x",
+            "4",
+            "-d",
+            "altodata/",
+            f"ftp://download.europeana.eu/newspapers/fulltext/alto/{id_}.zip",
+        ]
+    )
+    subprocess.call(["unzip", "altodata/*.zip", "-d", "altodata"])
+    [p.unlink() for p in Path("altodata").rglob("*.zip")]
+    subprocess.call(
+        [
+            "aria2c",
+            "-x",
+            "4",
+            "-d",
+            "altodata/metadata/",
+            f"ftp://download.europeana.eu/newspapers/metadata/{id_}.zip",
+        ]
+    )
+    subprocess.call(["unzip", "altodata/metadata/*.zip", "-d", "altodata/metadata/"])
+    [p.unlink() for p in Path("altodata/metadata").rglob("*.zip")]
+    alto_xmls = [f for f in Path("altodata").rglob("*.xml") if "edm" not in f.name]
+    datasets = process(alto_xmls, metadata_directory="altodata/metadata")
+    dataset = concatenate_datasets(datasets)
+    all_datasets.append(dataset)
+    [p.unlink() for p in Path("altodata").rglob("*.xml")]
+ds = concatenate_datasets(all_datasets)
+languages = set(concat(ds["language"]))
+decades = {f"{d[:3]}0" for d in ds["date"]}
+multi_language_ds = ds.filter(lambda x: x["multi_language"] == True)
+single_language_ds = ds.filter(lambda x: x["multi_language"] == False)
+for language in tqdm(languages):
+    lang_ds = single_language_ds.filter(lambda x: language in x["language"])
+    for decade in tqdm(decades, leave=False):
+        decade_ds = lang_ds.filter(
+            lambda x: f"{x['date'].split('-')[0][:3]}0" == decade
+        )
+        if len(decade_ds) > 0:
+            decade_ds.to_parquet(f"{language}-{decade}.parquet")
+for decade in tqdm(decades, leave=False):
+    decade_ds = multi_language_ds.filter(
+        lambda x: f"{x['date'].split('-')[0][:3]}0" == decade
+    )
+    if len(decade_ds) > 0:
+        decade_ds.to_parquet(f"multi_language-{decade}.parquet")
