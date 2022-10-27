8a3304c8e1a6884d6885356c7db18b94f33e714f
Daniel van Strien <davanstrien@gmail.com>
2022-10-27 16:57:49 +0100

    add all ids

--- load.py
+++ load.py
@@ -9,8 +9,8 @@ from tqdm.auto import tqdm
 
 from alto2dataset.europena import *
 
-# 9200300
-europena_ids = [9200396, 9200357, 9200339, 9200355, 9200356, 9200301, 9200338]
+
+europena_ids = [9200396, 9200357, 9200300, 9200339, 9200355, 9200356, 9200301, 9200338]
 
 
 all_datasets = []
