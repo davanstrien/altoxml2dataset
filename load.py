1e38f2316277f049e161dac2f8d780c26c194cc8
Daniel van Strien <davanstrien@gmail.com>
2022-10-29 08:51:26 +0100

    remove save

--- load.py
+++ load.py
@@ -77,7 +77,7 @@ for id_ in tqdm(europena_ids):
 parquet_files = Path(".").rglob("*.parquet")
 parquet_files = [str(file) for file in parquet_files]
 ds = datasets.Dataset.from_parquet(parquet_files)
-ds.save_to_disk("all_data")
+# ds.save_to_disk("all_data")
 languages = set(concat(ds["language"]))
 decades = {f"{d[:3]}0" for d in ds["date"]}
 multi_language_ds = ds.filter(lambda x: x["multi_language"] == True)
